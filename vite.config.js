a83bf4eccad3ef34e9d5105ac861d21513ef9083
Lucia <luciaseggiaro@gmail.com>
2024-07-29 22:26:07 +0200

    fix: vite seventh build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { hash } from "./src/utils/functions.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.svg'],
-  logLevel: 'info'
-})
+  assetsInclude: ["**/*.svg"],
+  logLevel: "info",
+  rollupOptions: {
+    output: {
+      entryFileNames: `[name]` + hash + `.js`,
+      chunkFileNames: `[name]` + hash + `.js`,
+      assetFileNames: `[name]` + hash + `.[ext]`,
+    },
+  },
+});
