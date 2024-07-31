299c93d8d98eef2bb0318cc08fcabcfd0e4587b8
Lucia <luciaseggiaro@gmail.com>
2024-07-31 14:49:07 +0200

    fix: footer social media links and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,31 @@ const Footer = () => {
       <div className="Footer-social">
         <span className="Footer-social-text">Share&#58;</span>
         <span className="Footer-social-networks">
-          <img src={instagram} alt="instagram" className="Footer-social-icon" />
-          <img src={facebook} alt="facebook" className="Footer-social-icon" />
-          <img src={twitter} alt="twitter" className="Footer-social-icon" />
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={instagram}
+              alt="instagram"
+              className="Footer-social-icon"
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={facebook} alt="facebook" className="Footer-social-icon" />
+          </a>
+          <a
+            href="https://www.twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={twitter} alt="twitter" className="Footer-social-icon" />
+          </a>
         </span>
       </div>
       <div className="Footer-copyright">
